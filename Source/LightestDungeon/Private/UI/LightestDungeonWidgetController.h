5a851bbe469954f40d1b9ae578435f6bcb067142
Fozzie-VR <51676947+Fozzie-VR@users.noreply.github.com>
2024-03-29 14:31:21 -0700

    Updates to add UI classes

---
+++ Source/LightestDungeon/Private/UI/LightestDungeonWidgetController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "LightestDungeonWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIGHTESTDUNGEON_API ULightestDungeonWidgetController : public UObject
+{
+	GENERATED_BODY()
+};
